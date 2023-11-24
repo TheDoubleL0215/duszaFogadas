c962041fba90e4a3371927ae107004ca5689a793
Levente <levai.levente.gabor@gmail.com>
2023-11-24 11:00:30 +0100

    mainMenu elkezdve

---
+++ src/main.py
@@ -0,0 +1,19 @@
+def mainMenu():
+    print(
+        '''
+        Üdvözlünk a fogadás oldalon!
+
+        Válassz az alábbi opciók közül:
+
+        1 -- Játék létrehozása
+        2 -- Fogadás leadása
+        3 -- Játék lezárása
+        4 -- Lekérdezések
+        5 -- Kilépés
+        
+        '''
+    )
+    mainChosenInput = int(input(">>"))
+
+
+mainMenu()
